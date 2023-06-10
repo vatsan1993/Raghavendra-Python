6186030461b4251865c4ba1003b0e4842ff0ea30
vatsan1993 <sozhavaramsrivatsan@hotmail.com>
2023-06-10 22:39:49 +0530

    added more programs.

--- debugSample.py
+++ debugSample.py
@@ -1,21 +1,29 @@
-# Check if every element is greater than 5.
-ratings = {
-    'laptop': 7,
-    'bread' : 8,
-    'toy' : 3,
-    'bell' : 2,
-    'desk' : 5,
-    'tv': 10
-}
-# cannot diretly use a dictionary with while loop
-# convert the dictionary into a list.
-items = list(ratings.keys())
-start = 0
-itemList = []
-while start < len(items):
-    key = items[start]
-    value = ratings[key]
-    if value > 5:
-        itemList.append(key)
-    start += 1
-print(itemList)
+word = input('Enter a string: ')
+index = 0
+newString = ''
+while index < len(word):
+#     // checking if the current character is z
+    if word[index] == 'z':
+#         // finding location of p. We are using the z location and finding the next occuring p
+        pos = word.find('p', index)
+        if pos != -1: # if p exists
+            # extracting the part between the z and p
+            substring = word[index +1  : pos]
+            # checking if substring has z inbetween
+            if 'z' in substring:
+                # we skip the current z and move to the next one.
+                newString += word[index]
+                index += 1
+            else:
+                # if z is not in substring, we delete the substring
+                newString += 'zp'
+                index = pos + 1
+        else:
+            # if p doesnt exist after z , moving to the next character.
+            newString += word[index]
+            index += 1
+    else:
+#         //if the current character is not z. adding the current character to the newString.
+        newString += word[index]
+        index += 1
+print(newString)
