1efdb63720d425c8fd53baea183b2d2c61b08795
vatsan1993 <sozhavaramsrivatsan@hotmail.com>
2023-06-30 10:04:57 +0530

    added programs

--- sample.py
+++ sample.py
@@ -1,20 +1,23 @@
-mylist = [1,2, 5,6,7, 9, 10, 11, 12, 20]
-largestLength = 1
-start = 0
-while start < len(mylist)-1:
-    start2 = start
-    count= 1
-    while start2 < len(mylist):
-        val1 = mylist[start2]
-        val2 = mylist[start2 + 1]
-        if val2  == val1 + 1:
-            count += 1
-        else:
-            if count > largestLength:
-                largestLength = count
-                count = 1
-                start = start2
+word = input('Enter a word: ')
+index = 0
+newString = ''
+pos = 0
+pos2 = 0
+found = False
+while index < len(word):
+    if word[index: index + 5] == 'bread':
+        pos = index + 5
+        pos2 = pos
+        while pos2 < len(word) - 4:
+            if word[pos2: pos2 + 5] == 'bread':
+                found = True
                 break
-        start2 += 1
-    start += 1
-print(largestLength)
+            pos2 += 1
+        if found:
+            break
+    index += 1
+
+if found:
+    print(word[pos: pos2])
+else:
+    print("Not a sandwitch")
