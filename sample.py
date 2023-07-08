7d1f29b21a206262e3e883932580ffd3b6b32dd6
vatsan1993 <sozhavaramsrivatsan@hotmail.com>
2023-07-08 11:34:20 +0530

    started chap 5

--- sample.py
+++ sample.py
@@ -1,23 +1,23 @@
-word = input('Enter a word: ')
-index = 0
-newString = ''
-pos = 0
-pos2 = 0
-found = False
-while index < len(word):
-    if word[index: index + 5] == 'bread':
-        pos = index + 5
-        pos2 = pos
-        while pos2 < len(word) - 4:
-            if word[pos2: pos2 + 5] == 'bread':
-                found = True
-                break
-            pos2 += 1
-        if found:
-            break
-    index += 1
+a = 10  # global variable
+def sampleFunction(b):
+    c = 20 # b and c are local
+    print(a) # no proble
+    print(b) # no problem
+    print(c) # no problem
+print(a) # no problem
+sampleFunction(40)
+print(b) # error
+print(c) # error
 
-if found:
-    print(word[pos: pos2])
-else:
-    print("Not a sandwitch")
+
+
+# def func1():
+#     a = 10
+#
+# def func2():
+#     func1()
+#
+# def func3():
+#     func2()
+#
+# func3()
