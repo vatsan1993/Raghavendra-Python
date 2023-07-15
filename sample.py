f3274d8ca15788d50e10dba6fcb07f4afda3c248
vatsan1993 <sozhavaramsrivatsan@hotmail.com>
2023-07-15 20:19:19 +0530

    added more programs

--- sample.py
+++ sample.py
@@ -1,23 +1,10 @@
-a = 10  # global variable
-def sampleFunction(b):
-    c = 20 # b and c are local
-    print(a) # no proble
-    print(b) # no problem
-    print(c) # no problem
-print(a) # no problem
-sampleFunction(40)
-print(b) # error
-print(c) # error
 
-
-
-# def func1():
-#     a = 10
-#
-# def func2():
-#     func1()
-#
-# def func3():
-#     func2()
-#
-# func3()
+def changeList2(data):# data = @ 840
+    l2 = data.copy() #l2 = @ 920
+    print(f'The data inside the function before change {data}')
+    l2[0] = 100
+    print(f'The data inside the function after change {data}')
+l1 = [1,2,3] # @ 840
+print('before calling function',l1)
+changeList2(l1)
+print('after calling function',l1)
