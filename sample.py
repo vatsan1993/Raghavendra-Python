005ec74ea46afd726146dfc5bb70ca2f202b84e8
vatsan1993 <sozhavaramsrivatsan@hotmail.com>
2023-07-19 10:35:10 +0530

    added more programs

--- sample.py
+++ sample.py
@@ -1,10 +1,8 @@
+# factorial
+def fact(n):
+    # base case
+    if n <= 0:
+        return 1
+    return n * fact(n-1)
 
-def changeList2(data):# data = @ 840
-    l2 = data.copy() #l2 = @ 920
-    print(f'The data inside the function before change {data}')
-    l2[0] = 100
-    print(f'The data inside the function after change {data}')
-l1 = [1,2,3] # @ 840
-print('before calling function',l1)
-changeList2(l1)
-print('after calling function',l1)
+print(fact(5))
